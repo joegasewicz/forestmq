584c47d0d202164fc3688d3afe2b247bfd4234f5
joegasewicz <joegasewicz@gmail.com>
2026-03-18 19:34:12 +0000

    #11

--- src/message.zig
+++ src/message.zig
@@ -1,6 +1,21 @@
 
 pub const Message = struct {
+
+    const Self = @This();
+
     topic: []const u8,
     body: []const u8,
     flags: u8 = 0,
+
+    pub fn init(
+        topic: []const u8,
+        body: []const u8,
+        flags: []const u8,
+    ) Self {
+        return Self{
+            .topic = topic,
+            .body = body,
+            .flags = flags,
+        };
+    }
 };
