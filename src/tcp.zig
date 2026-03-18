798e5f784a3ff73b1a529ab2d956c2ccc260eba7
joegasewicz <joegasewicz@gmail.com>
2026-03-18 23:05:37 +0000

    #13

--- src/tcp.zig
+++ src/tcp.zig
@@ -1,2 +1,27 @@
 const std = @import("std");
 
+const log = std.log;
+
+pub const TCP = struct {
+    const Self = @This();
+
+    host: []const u8,
+
+    pub fn init(host: []const u8) Self {
+        return Self {
+            .host = host,
+        };
+    }
+
+    // pub fn deinit(self: *Self) void {
+    //
+    // }
+
+    pub fn serve(self: *Self) !void {
+        const addr = try std.net.Address.parseIp(self.host, 8752);
+        var server = try std.net.Address.listen(addr, .{ .reuse_address = true });
+        defer server.deinit();
+
+        log.info("Starting server on {f}", .{addr});
+    }
+};
