eec13b616bba0830a32fef6328bf282bee764ac2
joegasewicz <joegasewicz@gmail.com>
2026-03-18 20:17:23 +0000

    #12

--- src/forestmq.zig
+++ src/forestmq.zig
@@ -1,20 +1,33 @@
 const std = @import("std");
+const topic_module = @import("./topic.zig");
+
+const Topic = topic_module.Topic;
 
 pub const ForestMQ = struct {
     const Self = @This();
 
     allocator: std.mem.Allocator,
+    topics: Topic,
 
     pub fn init(allocator: std.mem.Allocator) !Self {
        return Self{
-        .allocator = allocator,
+            .allocator = allocator,
+            .topics = try Topic.init(allocator),
        };
     }
+
+    pub fn deinit(self: *Self) void {
+        self.topics.deinit();
+        self.* = undefined;
+    }
+
 };
 
 
 test "ForestMQ.init success" {
-    const fq = try ForestMQ.init(std.testing.allocator);
-    _ = fq;
+    var fq = try ForestMQ.init(std.testing.allocator);
+    defer fq.deinit();
+
+    try std.testing.expect(fq.topics.topic_map.count() == 0);
 }
 
